becabb4341ff7262a19185d5a2b8ef40bb8d44d5
Mathieu Rochette <mathieu@rochette.cc>
2015-01-03 22:39:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Docker Virtual Hosts
+
+docker-virtual-hosts sets up a container running a web service listing your docker virtual hosts. It is intended to be used with [nginx-proxy][1] but it can also be used on its own.
+
+## Usage
+
+To run it:
+
+    $ docker run -it -e VIRTUAL_HOST=dev.127.0.0.1.xip.io -v /var/run/docker.sock:/tmp/docker.sock --name virtual_hosts texthtml/docker-vhosts
+
+If `nginx-proxy` is running and listening on 127.0.0.1, you can visit http://dev.127.0.0.1.xip.io/ to see your other(s) virtual host(s).
+
+Every containers with a `VIRTUAL_HOST=...` env var set should be present. You can also add labels to your containers with `VIRTUAL_LABELS=some spaces separated labels`. You we will then be able to filter yout vhosts using those labels.
---
+++ app/www/index.js
@@ -0,0 +1,73 @@
+(function () {
+	angular.module('VirtualHostsApp', [])
+	.controller('VirtualHostsController', ['$http', '$interval', function($http, $interval) {
+		var activeLabels = [];
+		var hosts = [];
+		var labels = [];
+
+		var unique = function(array) {
+			return array.filter(function(value, index, self) {
+				return self.indexOf(value) === index;
+			});
+		};
+
+		this.hosts = function() {
+			return hosts;
+		};
+
+		this.labels = function() {
+			return labels;
+		};
+
+		this.containerLabels = function(containers) {
+			var labels = [];
+			angular.forEach(containers, function(container) {
+				this.push.apply(this, container.labels);
+			}, labels);
+			return unique(labels);
+		};
+
+		this.containersNames = function(containers) {
+			var containersNames = [];
+			angular.forEach(containersNames, function(container) {
+				this.push(container.name);
+			}, containers);
+			return containersNames;
+		};
+
+		this.labelIsSelected = function(label) {
+			return activeLabels.indexOf(label) !== -1;
+		};
+
+		this.toggleLabel = function(label) {
+			var pos = activeLabels.indexOf(label);
+			if (pos === -1) {
+				activeLabels.push(label);
+				return;
+			}
+			activeLabels.splice(pos, 1);
+		};
+
+		this.containersHaveSelectedLabels = function(containers) {
+			var labels = this.containerLabels(containers);
+			for (var i = 0; i < activeLabels.length; i++) {
+				var activeLabel = activeLabels[i];
+				if (labels.indexOf(activeLabel) === -1) {
+					return false;
+				}
+			}
+			return true;
+		};
+
+		var updateHosts = function() {
+			$http.get('/hosts.json').then(function(response) {
+				hosts = response.data.hosts;
+				labels = response.data.labels;
+			});
+		};
+
+		$interval(updateHosts, 5000);
+
+		updateHosts();
+	}])
+}) (angular);
